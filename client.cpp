07d73b5b62e3cc39778343794967cd0d6c8d211b
Adam Šulc <xsulca00@stud.fit.vutbr.cz>
2018-03-10 20:53:30 +0100

    Read hopefully works

--- client.cpp
+++ client.cpp
@@ -68,10 +68,8 @@ Target to(Source arg)
 using Buffer = array<char, 4096>;
 
 struct Chunk {
-    Chunk(const Buffer& d, int s) : data{d}, size{s} {}
-
     Buffer data;
-    long size;
+    long size {};
 };
 
 void check_arguments(const Args& a) {
@@ -79,57 +77,6 @@ void check_arguments(const Args& a) {
     if (a.port.empty()) throw runtime_error{"main(): port not set!"};
     if (a.flags == Flags::none) throw runtime_error{"main(): [-w|-r] not set!"};
     if (a.file.empty()) throw runtime_error{"main(): file not set!"};
-}
-
-int create_socket() {
-    int client {socket(AF_INET, SOCK_STREAM, 0)};
-    if (client <= 0) throw runtime_error{"Cannot create socket!"};
-    return client;
-}
-
-long check_response_and_get_filesize(const string& response) {
-    size_t found {response.find("96 Begin file transfer. Size: ")};
-    if (found == string::npos) throw runtime_error{"Invalid begin to transfer command sent by server: \"" + response + '"'};
-
-    string file_size {response.substr(found+strlen("96 Begin file transfer. Size: "))};
-    if (file_size.size() >= 2 && 
-       (file_size.back() != '\n' ||  file_size[file_size.size()-2] != '\r')) throw runtime_error{"Invalid begin to transfer command sent by server: \"" + response + '"'};
-
-    return stol(file_size);
-}
-
-bool recieve_bytes(int socket, Buffer& b) {
-    ssize_t bytes {recv(socket, b.data(), b.size(), 0)};
-    if (bytes < 0) perror("recv");
-    return bytes < 0;
-}
-
-void read_file_from_server(int socket) {
-    Buffer buffer;
-
-    recieve_bytes(socket, buffer);
-
-    ssize_t file_size {check_response_and_get_filesize(buffer.data())};
-
-    vector<Chunk> data;
-    for (ssize_t i {1}; i <= file_size; i += bytes) {
-        recieve_bytes(socket, buffer);
-        data.emplace_back(buffer, bytes);
-    }
-
-    {
-        ofstream file {"file", ios_base::binary};
-        if (!file) throw runtime_error{"Cannot open file"};
-        for (const Chunk& c : data) {
-            file.write(c.data.data(), c.size);
-        }
-    }
-
-    recieve_bytes(socket, buffer);
-
-    if (string{buffer.data(), buffer.size()}.find("97 File transfer completed\r\n") != string::npos) {
-        throw runtime_error{"Server did not confirmed that file transfer is completed!"};
-    }
 }
 
 string make_command(Flags f, const string& file) {
@@ -142,7 +89,105 @@ string make_command(Flags f, const string& file) {
     return command += ' ' + file + "\r\n";
 }
 
-int main(int argc, char* argv[]) {
+
+int create_socket() {
+    int client {socket(AF_INET, SOCK_STREAM, 0)};
+    if (client <= 0) throw runtime_error{"Cannot create socket!"};
+    return client;
+}
+
+void send_request(int socket, const string& command) {
+    ssize_t bytes {send(socket, command.c_str(), command.length(), 0)};
+    if (bytes < 0) perror("sendto");
+}
+
+string recieve_response(int socket, ssize_t len) {
+    string s(len, '\0');
+
+    ssize_t bytes {};
+    char* b {&s[0]};
+    for (ssize_t remainder {len}; remainder > 0; remainder -= bytes) {
+        bytes = recv(socket, b, remainder, 0);
+        if (bytes < 0) perror("recv");
+        b += bytes;
+    }
+
+    return s;
+}
+
+struct Header {
+    int length {};
+    bool last {};
+};
+
+Header get_header(int socket) {
+    ssize_t bytes {};
+    Header h;
+    for (ssize_t remainder {sizeof(h)}; remainder > 0; remainder -= bytes) {
+        bytes = recv(socket, &h, remainder, 0);
+        if (bytes < 0) perror("recv");
+    }
+    return h;
+}
+
+void get_chunk(int socket, Buffer& b, ssize_t len) {
+    ssize_t bytes {};
+    for (ssize_t remainder {len}; remainder > 0; remainder -= bytes) {
+        bytes = recv(socket, b.data(), remainder, 0);
+        if (bytes < 0) perror("recv");
+    }
+}
+
+vector<Chunk> recieve_file(int socket) {
+    vector<Chunk> chunks;
+    Header h;
+    Chunk c;
+    while (!h.last) {
+        h = get_header(socket);
+        if (h.length <= 0) throw runtime_error{"Header length=(" + to_string(h.length) + ") <= 0"};
+
+        chunks.push_back(Chunk{});
+        get_chunk(socket, chunks.back().data, h.length);
+        chunks.back().size = h.length;
+    }
+    return chunks;
+}
+
+void write_data_to_file(const string& name, const vector<Chunk>& data) {
+    ofstream file {name, ios_base::binary};
+    if (!file) throw runtime_error{"Cannot open file: \"" + name + '"'};
+    for (const Chunk& c : data) file.write(c.data.data(), c.size);
+}
+
+void read_file_from_server(int socket, const Args& a) {
+    const string begin_file_transfer {"100 Begin file transfer\r\n"};
+    const string cannot_open_file {"101 Cannot open file\r\n"};
+
+    send_request(socket, make_command(Flags::read, a.file));
+    string response {recieve_response(socket, begin_file_transfer.length())};
+
+    if (response == begin_file_transfer)  {
+        vector<Chunk> file_data {recieve_file(socket)};
+        write_data_to_file(a.file, file_data);
+
+        const string file_transfer_completed {"97 File transfer completed\r\n"};
+        string response1 {recieve_response(socket, file_transfer_completed.length())};
+        if (response1 != file_transfer_completed) {
+            throw runtime_error{"Server did not confirmed that file transfer is completed, this message was sent instead: " + response1}; 
+        }
+        return;
+    }
+
+    if (response == cannot_open_file) {
+        cout << "File \"" << a.file << "\" cannot be transmitted\n";
+        return;
+    } 
+
+    throw runtime_error{"Server sent invalid response: " + response};
+}
+
+int main(int argc, char* argv[]) 
+try {
     Args a {args(argc, argv)};
     check_arguments(a);
 
@@ -161,25 +206,20 @@ int main(int argc, char* argv[]) {
 
     string command {make_command(a.flags, a.file)};
 
-    cout << "Sending command: \"" << command << "\"\n";
-
-    // sending command
-    ssize_t bytes {send(client, command.c_str(), command.length(), 0)};
-    if (bytes < 0) perror("sendto");
+    cout << "Sending command: " << command; 
 
     // choose action
     if (a.flags == Flags::write) {
     } else if (a.flags == Flags::read) {
-        read_file_from_server(client);
-    }
+        read_file_from_server(client, a);
+    } 
+} catch (const exception& e) {
+    string s {e.what()};
+    auto pos = s.find_last_of('\r');
+    if (pos != string::npos) s.insert(pos, "\\r");
+    pos = s.find_last_of('\n');
+    if (pos != string::npos) s.insert(pos, "\\n");
 
-    /*
-    // wait for reply
-    bytes = send(client, command.c_str(), command.length(), 0);
-    if (bytes < 0) perror("sendto");
-
-    // recieve bytes 
-    bytes = recv(client, buffer.data(), buffer.size(), 0);
-    if (bytes < 0) perror("recv");
-    */
+    cerr << "Exception has been thrown: " << s << '\n';
+    return 1;
 }
